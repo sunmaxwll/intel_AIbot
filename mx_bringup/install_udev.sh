80ad46b62a2b765f990f7eab91d6d706a28a4768
sunMaxwell <sunmaxwell@outlook.com>
2018-06-07 17:20:34 +0800

    remove some useless files

--- mx_bringup/install_udev.sh
+++ mx_bringup/install_udev.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-echo "remap the device serial port(ttyUSBX) to rplidar mx_sensor and mx_chassis"
+echo "remap the device serial port(ttyUSBX) to rplidar and arduino"
 echo "check it using the command : ls -l /dev|grep ttyUSB"
 echo "start copy rplidar.rules to  /etc/udev/rules.d/"
 echo "start copy arduino.rules to  /etc/udev/rules.d/"
-echo "start copy mx_chassis.rules to  /etc/udev/rules.d/"
-echo "start copy mx_sensor.rules to  /etc/udev/rules.d/"
 echo "start copy 56-orbbec-usb.rules to  /etc/udev/rules.d/"
 
-sudo cp ./_udev_/rplidar.rules  /etc/udev/rules.d
-sudo cp ./_udev_/mx_chassis.rules  /etc/udev/rules.d
-sudo cp ./_udev_/mx_sensor.rules  /etc/udev/rules.d
-sudo cp ./_udev_/56-orbbec-usb.rules  /etc/udev/rules.d
+sudo cp ./_udev_/*.rules  /etc/udev/rules.d
+
 echo " "
 echo "Restarting udev"
 echo ""
 sudo service udev reload
 sudo service udev restart
 echo "finish "
-echo "BY Maxwell AT:2017.10.16"
+echo "BY Maxwell AT:2017.06.07"
